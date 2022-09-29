23b8f65c91af4efc82515657fd114bd2fa837745
kunj <kunj@kpkunj>
2022-09-29 01:12:21 -0400

    Script1 sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #printing the FQDN using hostname command with help of echo
-echo $HOSTNAME
+echo "FQDN:"$HOSTNAME
 #using uname command to print the type of operating system and its version.
 #m for machine name, R for kernel release and s for kernel name. 
-echo $(uname -mrs)
+echo "Machine name:"$(uname -m)
+echo "Kernel release:"$(uname -r)
+echo "Kernel namw:"$(uname -s)
 #command to list ip address of the host. 
-echo $(hostname -I)
+echo "IP address:"$(hostname -I)
 #command to display space available in root filesystem
-echo $(df -h /)
+echo "Root filesystem status:"$(df -h /)
 
